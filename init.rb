2f1879a926db143bc9a607aba89769bf4def7787
Lukas Eklund <leklund@gmail.com>
2013-04-25 12:24:56 -0400

    multiapps option

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
 require 'multiapp/heroku/command/multiapp'
+
+Heroku::Command.global_option :multiapps, "--multiapps app1,app2,app3"
--- lib/multiapp/heroku/command/multiapp.rb
+++ lib/multiapp/heroku/command/multiapp.rb
@@ -2,11 +2,17 @@ require 'heroku/command/base'
 
 class Heroku::Command::Multiapp < Heroku::Command::Base
   def index
-    apps = api.get_apps.body
+    apps = api.get_apps.body.map{|a| a["name"]}
     args = ARGV[1..-1]
     cmd = args.shift
+    if options[:multiapps]
+      options[:multiapps].split(',').each do |app|
+        error("Invalid app name '#{app}' passed in multiapps option") unless apps.include?(app)
+      end
+      apps = options[:multiapps].split(',')
+    end
     apps.each do |app|
-      Heroku::Command.run(cmd, args + ['--app',app["name"]])
+      Heroku::Command.run(cmd, args + ['--app',app])
     end
   end
 end
